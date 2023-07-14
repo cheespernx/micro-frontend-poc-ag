a74183e73f7c63096cb58fab721be47d41589b57
Victor "Cheesper" Carvalho <contato@cheesper.com>
2023-07-14 00:05:20 -0400

    Update styles.ts

--- src/styles.ts
+++ src/styles.ts
@@ -28,6 +28,7 @@ export const FormRow = styled.div`
     flex-direction: column;
     align-items: center;
     justify-content: center;
+    margin-bottom: 0;
   }
 `
 
@@ -44,7 +45,7 @@ export const FormDiv = styled.div<Props>`
 
   @media screen and (max-width: 768px) {
     margin: 0;
-    margin-bottom: 1rem;
+    margin-bottom: 24px;
     display: flex;
     align-items: center;
     justify-content: center;
